a5e5caea5420153af0daaf0f6a07f10baf1597a2
grahamlyons <graham@grahamlyons.com>
2017-07-14 15:37:26 +0100

    Pass a timeout for the lamda function

--- main.tf
+++ main.tf
@@ -5,6 +5,7 @@ resource "aws_lambda_function" "lambda_function" {
   role          = "${aws_iam_role.iam_for_lambda.arn}"
   handler       = "${var.handler}"
   runtime       = "${var.runtime}"
+  timeout       = "${var.timeout}"
 
   environment {
     variables = "${var.lambda_env}"

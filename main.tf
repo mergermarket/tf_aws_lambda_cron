3cdc2e1141ff2abc4ac9b373ca75b214c212fbb4
grahamlyons <graham@grahamlyons.com>
2017-07-14 16:21:36 +0100

    Revert "Try to make sure that these are lists"
    This reverts commit 9ef6c222115b3712bf14fb5e0ce877a69b969750.

--- main.tf
+++ main.tf
@@ -8,8 +8,8 @@ resource "aws_lambda_function" "lambda_function" {
   timeout       = "${var.timeout}"
 
   vpc_config {
-    subnet_ids         = "${list(var.subnet_ids)}"
-    security_group_ids = "${list(var.security_group_ids)}"
+    subnet_ids         = "${var.subnet_ids}"
+    security_group_ids = "${var.security_group_ids}"
   }
 
   environment {

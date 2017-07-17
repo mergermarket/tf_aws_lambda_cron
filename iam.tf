6ef57bf5a298ef83d7631c80ae66d56601ca6f6a
grahamlyons <graham@grahamlyons.com>
2017-07-17 10:49:33 +0100

    Give VPC access policy if required

--- iam.tf
+++ iam.tf
@@ -1,3 +1,11 @@
+resource "aws_iam_role_policy_attachment" "vpc_permissions" {
+  name       = "${var.function_name}-vpc_permissions"
+  roles      = ["${aws_iam_role.iam_for_lambda.name}"]
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
+
+  count      = "${length(var.subnet_ids)} != 0 ? 1 : 0"
+}
+
 resource "aws_iam_role" "iam_for_lambda" {
   assume_role_policy = <<EOF
 {

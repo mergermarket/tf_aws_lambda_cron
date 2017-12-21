b7101b802a371d9d7ba98cb78c8c06248a740d27
grahamlyons <graham@grahamlyons.com>
2017-12-21 15:38:36 +0000

    We actually need to role name for attachments

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,6 @@ output "lambda_arn" {
   value = "${aws_lambda_function.lambda_function.arn}"
 }
 
-output "lambda_iam_role_arn" {
-  value = "${aws_iam_role.iam_for_lambda.arn}"
+output "lambda_iam_role_name" {
+  value = "${aws_iam_role.iam_for_lambda.name}"
 }
